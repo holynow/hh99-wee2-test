5dcef9a33fc1b71582a0c5bbdea48efef5965886
함석원 <s1@hamseog-won-ui-Macmini.local>
2024-02-26 11:34:41 +0900

    Design: css 수정

--- src/App.js
+++ src/App.js
@@ -1,37 +1,6 @@
 import React, { useState } from 'react';
-import './reset.css';
-import { v4 as uuidv4 } from 'uuid';
-
-const wrap = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: "30px",
-  width: "100%",
-  padding: "100px 30px",
-  textAlign: "center"
-}
-const h2 = {
-  fontSize: "30px",
-  fontWeight: "800"
-}
-const listWrap = {
-  width: "100%",
-}
-const ul = {
-  display: "flex",
-  gap: "20px",
-  width: "100%",
-  marginTop: "20px"
-}
-const li = {
-  width: "100px",
-  height: "100px",
-  padding: "15px",
-  border: "1px solid #2fa339",
-  borderRadius: "10px"
-}
+import {v4 as uuidv4} from 'uuid';
+import './App.css'
 
 const App = () => {
   const [toDo, setTodo] = useState([])
@@ -40,7 +9,7 @@ const App = () => {
     setTitle(event.target.value);
   }
   const onClickAddList = () => {
-    if (title.trim() !== '') {
+    if(title.trim() !== ''){
       const newTodo = {
         id: uuidv4(),
         title,
@@ -49,19 +18,19 @@ const App = () => {
     }
   }
   return (
-    <div style={wrap}>
+    <div className='wrap'>
       <div>
-        <input type="text" value={title} onChange={onChangeTitle} />
+        <input type="text" value={title} onChange={onChangeTitle}/>
         <button onClick={onClickAddList}>추가하기</button>
       </div>
-      <div style={listWrap}>
-        <h2 style={h2}>Todo List</h2>
-        <ul style={ul}>
-          {toDo.map((item) => {
-            return (
-              <li key={item.id} style={li}>
-                <p>{item.title}</p>
-              </li>
+      <div className='list-wrap'>
+        <h2 className="title">Todo List</h2>
+        <ul className='list-group'>
+          {toDo.map((item)=> {
+            return(
+            <li key={item.id} className='list-item'>
+              <p>{item.title}</p>
+            </li>
             )
           })}
         </ul>
